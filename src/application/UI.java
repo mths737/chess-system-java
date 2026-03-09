82168830c3bbeea294c3a275292207ddb2cc753a
Matheus Alves <mths737@gmail.com>
2026-03-09 07:38:00 -0300

    Handling exceptions and clearing screen

--- src/application/UI.java
+++ src/application/UI.java
@@ -29,6 +29,11 @@ public class UI {
 	public static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
 	public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
 	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
+	
+	public static void clearScreen() {
+		System.out.print("\033[H\033[2J");
+		System.out.flush();
+	}
 
 	public static ChessPosition readChessPosition(Scanner sc) {
 		try {
@@ -37,7 +42,7 @@ public class UI {
 			int row = Integer.parseInt(s.substring(1));
 			return new ChessPosition(collumn, row);
 		} catch (RuntimeException e) {
-			throw new InputMismatchException("Error reading ChesPosition. Valid values re from a1 to h8");
+			throw new InputMismatchException("Error reading ChesPosition. Valid values are from a1 to h8");
 		}
 	}
 
